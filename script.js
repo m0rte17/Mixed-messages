985a21d6d91ba9eb82fad36382d73354ce869030
m0rte17 <alexgrudko@gmail.com>
2024-08-06 13:28:29 +0300

    Added arrays

---
+++ script.js
@@ -0,0 +1,28 @@
+const beginnings = [
+    "Every day is a new opportunity",
+    "Don't be afraid of big goals",
+    "Your self-confidence",
+    "Focus on your dreams",
+    "Your perseverance"
+]; 
+
+const middles = [
+    "will lead you to success",
+    "will help you overcome all obstacles",
+    "is the key to achievement",
+    "will open doors to new opportunities",
+    "will make the impossible possible"
+];
+
+const endings = [
+    "and bring you many joyful moments.",
+    "and allow you to reach incredible heights.",
+    "and change your life for the better.",
+    "and bring satisfaction from every goal achieved.",
+    "and turn your dreams into reality."
+];
+
+function getRandomElement(arr) {
+    return arr[Math.floor(Math.random) * arr,length];
+}
+
